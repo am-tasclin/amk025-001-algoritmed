b667e865c4217ddb58fd55eae711f40633cad649
romanm <roman.mishchenko@gmail.com>
2018-12-22 17:30:17 +0200

    first commit

---
+++ src/main/java/org/algoritmed/amk025/Amk025001AlgoritmedApplication.java
@@ -0,0 +1,14 @@
+package org.algoritmed.amk025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amk025001AlgoritmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amk025001AlgoritmedApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/algoritmed/amk025/Amk025001AlgoritmedApplicationTests.java
@@ -0,0 +1,17 @@
+package org.algoritmed.amk025;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Amk025001AlgoritmedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
