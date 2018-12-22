415228aeaf77ddf3f03e3c2baffd7c4deaf085f8
romanm <roman.mishchenko@gmail.com>
2018-12-22 23:50:49 +0200

    add DB

--- src/main/java/org/algoritmed/amk025/Amk025001AlgoritmedApplication.java
+++ src/main/java/org/algoritmed/amk025/Amk025001AlgoritmedApplication.java
@@ -2,8 +2,10 @@ package org.algoritmed.amk025;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:config-app-spring.xml")
 public class Amk025001AlgoritmedApplication {
 
 	public static void main(String[] args) {
